d2b5e508f4eb1d58f275d653a9359f66d71dc27a
MahinKannan <1mahink@gmail.com>
2020-07-28 18:01:38 -0500

    init commit

---
+++ precache-manifest.22665e0a5716b45bf8220767f6010406.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d588ff4c348a66db444746a2493d7177",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9b4dac0647f0d482f8",
+    "url": "/static/css/main.80a3fa70.chunk.css"
+  },
+  {
+    "revision": "a821b3241ca7beb5489b",
+    "url": "/static/js/2.11c56c60.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.11c56c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9b4dac0647f0d482f8",
+    "url": "/static/js/main.a9e8c525.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
